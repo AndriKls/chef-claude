4711ab06c8bea2eec5db6c6884d7d6f444bcb4fe
AndriKls <158137294+AndriKls@users.noreply.github.com>
2025-01-08 16:56:36 +0200

    Created dynamic content blocks to show list ingredients and get recipe button

--- components/Main.jsx
+++ components/Main.jsx
@@ -11,13 +11,14 @@ export default function Main() {
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
-        
     }
+    
+
 
     return (
         <main>
             <form action={addIngredient} className="add-ingredient-form">
-                <input 
+                <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
@@ -25,9 +26,17 @@ export default function Main() {
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsListItems}
-            </ul>
+            {(ingredientsListItems.length) > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                {ingredients.length >= 3 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>}
+            </section>}
         </main>
     )
 }
